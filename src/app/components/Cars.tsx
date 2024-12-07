bb9e944f79782ad7d3af3531947a51508e8f6f36
SAAD-NADEEM <saadngaming99090@gmail.com>
2024-12-07 23:00:42 +0500

    errors fix

--- src/app/components/Cars.tsx
+++ src/app/components/Cars.tsx
@@ -6,7 +6,7 @@ function Cars({cars}:CarsProps) {
     return (
         <section className="flex flex-col lg:flex-row gap-5 h-auto">
             {cars.map((car, index) => (
-                <Car img={car.img} carName={car.carName} carPrice={car.carPrice} Reviews={car.Reviews}/>
+                <Car key={index} img={car.img} carName={car.carName} carPrice={car.carPrice} Reviews={car.Reviews}/>
             ))}
         </section>
     )
